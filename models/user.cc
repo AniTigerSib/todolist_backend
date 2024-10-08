7dae4377fe5600adec29d20980811b96959d569e
Michael <khudoleev04@mail.ru>
2024-10-09 02:36:52 +0500

    User/More fixes, methods for work with passwords has been added.

--- models/user.cc
+++ models/user.cc
@@ -10,14 +10,16 @@ std::regex const User::passwordRegex_ = std::regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*
 
 // +++++++++++++++++ Initialization +++++++++++++++++ //
 
-User::User(const drogon::orm::Row& row) {
+User::User(const drogon::orm::Row& row)
+{
     id_ = row["id"].as<int>();
     login_ = std::move(row["login"].as<std::string>());
     email_ = std::move(row["email"].as<std::string>());
     password_ = std::move(row["password"].as<std::string>());
 }
 
-void User::validateUserData(const char *login, const char *email, const char *password) {
+void User::validateUserData(const char *login, const char *email, const char *password)
+{
     std::cmatch m;
 
     if (login != nullptr && (strlen(login) > loginSizeMax_ || !std::regex_match(login, m, loginRegex_)))
@@ -28,51 +30,59 @@ void User::validateUserData(const char *login, const char *email, const char *pa
         throw UserException("Invalid password format");
 }
 
-User::User(const char* login, const char* email, const char* password) {
+User::User(const char* login, const char* email, const char* password)
+{
     if (login == nullptr || email == nullptr || password == nullptr)
         throw ServerException("Invalid user data");
     validateUserData(login, email, password);
 
     login_ = std::string(login);
     email_ = std::string(email);
-    password_ = std::string(password);
+    password_ = std::move(hashPassword(std::string(password)));
     id_ = -1;
 }
 
-User::User(const std::string& login, const std::string& email, const std::string& password) {
+User::User(const std::string& login, const std::string& email, const std::string& password)
+{
     validateUserData(login.c_str(), email.c_str(), password.c_str());
 
     login_ = login;
     email_ = email;
-    password_ = password;
+    password_ = std::move(hashPassword(std::string(password)));
     id_ = -1;
 }
 
-User::User(std::string login, std::string email, std::string password) {
+User::User(std::string login, std::string email, std::string password)
+{
     validateUserData(login.c_str(), email.c_str(), password.c_str());
 
     login_ = std::move(login);
     email_ = std::move(email);
-    password_ = std::move(password);
+    password_ = std::move(hashPassword(std::string(std::move(password))));
     id_ = -1;
 }
 
 // ----------------- Initialization ----------------- //
 
 
+// +++++++++++++++++ Main methods +++++++++++++++++ //
 
-std::future<drogon::orm::Result> User::getGetByLoginAsyncFutureSqlExec(const drogon::orm::DbClientPtr& clientPtr, std::string& login) {
+std::future<drogon::orm::Result> User::getUserByLoginAsyncFutureSqlExec(const drogon::orm::DbClientPtr& clientPtr, std::string& login)
+{
     validateUserData(login.c_str(), nullptr, nullptr);
 
     return clientPtr->execSqlAsyncFuture("select id, login, email, password from users where login=$1", login);
 }
 
-User User::getByLogin(const drogon::orm::DbClientPtr& clientPtr, std::string& login) {
-    auto result = getGetByLoginAsyncFutureSqlExec(clientPtr, login).get();
+User User::getUserByLogin(const drogon::orm::DbClientPtr& clientPtr, std::string& login)
+{
+    const auto result = getUserByLoginAsyncFutureSqlExec(clientPtr, login).get();
 
     if (result.empty())
         throw UserException("User not found");
 
-    auto res = result[0];
+    const auto res = result[0];
     return std::move(User(res));
 }
+
+// ----------------- Main methods ----------------- //
--- models/user.h
+++ models/user.h
@@ -12,6 +12,8 @@
 class User;
 class UserException;
 
+// TODO: Уточнить, выгоднее ли сначала проверять валидность данных, а уже потом создавать объект
+
 class User {
 public:
     // Constructors
@@ -23,20 +25,36 @@ public:
     // Getters and setters
     [[nodiscard]] const std::string& getLogin() const { return login_; }
     [[nodiscard]] const std::string& getEmail() const { return email_; }
-    [[nodiscard]] const std::string& getPassword() const { return password_; }
     [[nodiscard]] int getId() const { return id_; }
+
+    // Password work
+    [[nodiscard]] bool comparePassword(const std::string& password) const { return password_ == password; }
+    static std::string hashPassword(const std::string& password)
+    {
+        return std::move(drogon::utils::getSha256(password));
+    }
+    static std::string hashPasswordWithSalt(const std::string& password, const std::string& salt)
+    {
+        return std::move(drogon::utils::getSha256(password + salt));
+    }
+
 
     // Static methods
     static void validateUserData(const char *login, const char *email, const char *password);
-    static std::future<drogon::orm::Result> getGetByLoginAsyncFutureSqlExec(const drogon::orm::DbClientPtr& clientPtr, std::string& login);
-    static User getByLogin(const drogon::orm::DbClientPtr& clientPtr, std::string& login);
 
-    // std::future<const drogon::orm::Result>  getInsertAsyncFutureSqlExec();
+    // Database getters
+    static std::future<drogon::orm::Result> getUserByLoginAsyncFutureSqlExec(const drogon::orm::DbClientPtr& clientPtr, std::string& login);
+    static User getUserByLogin(const drogon::orm::DbClientPtr& clientPtr, std::string& login);
+
+    // Database setters
+    // std::future<const drogon::orm::Result> getInsertAsyncFutureSqlExec(const drogon::orm::DbClientPtr& clientPtr);
     // std::future<const drogon::orm::Result>  getUpdateAsyncFutureSqlExec();
 
 private:
     int id_;
     std::string login_, email_, password_;
+
+    // Constants
     static constexpr int loginSizeMax_ = 20;
     static constexpr int emailSizeMax_ = 30;
     static std::regex const loginRegex_;
